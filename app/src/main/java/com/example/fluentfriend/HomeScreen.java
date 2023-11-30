591ec479cfe696a562ed34b25adf15a317fdd570
move18 <snoozie@hotmail.se>
2023-11-30 12:43:05 +0100

    First changes

--- app/src/main/java/com/example/fluentfriend/HomeScreen.java
+++ app/src/main/java/com/example/fluentfriend/HomeScreen.java
@@ -1,10 +1,25 @@
 package com.example.fluentfriend;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.annotation.Nullable;
 
-import android.app.Application;
 
-public class HomeScreen extends Application {
-
+public class HomeScreen extends Activity {
 
 
+    Button btn;
 
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+        btn = findViewById(R.id.homeMatchBtn);
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
--- app/src/main/java/com/example/fluentfriend/MainActivity.java
+++ app/src/main/java/com/example/fluentfriend/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.fluentfriend;
 
 import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button signInBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signInBtn = (Button) findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, HomeScreen.class);
+            startActivity(intent);
+
+        });
     }
 }
