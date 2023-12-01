58583a1e239bcf14cdf8fd88bca0db21afcb37cd
Goosedawg <120063500+Goosedawg@users.noreply.github.com>
2023-12-01 10:33:01 +0100

    här kommer user-klassen!

--- app/src/main/java/com/example/fluentfriend/User.java
+++ app/src/main/java/com/example/fluentfriend/User.java
@@ -5,11 +5,29 @@ public class User {
     private String lastName;
     private String eMail;
     private String password;
+    private String bio;
     private boolean fika;
     private boolean museum;
     private boolean cityWalks;
     private boolean barChill;
 
+    public void registerNewUser(String firstname, String lastName, String eMail, String password) {
+        setFirstName(firstname);
+        setLastName(lastName);
+        setEmail(eMail);
+        setPassword(password);
+    }
+
+    public void setUserInterests(boolean fika, boolean museum, boolean cityWalks, boolean barChill){
+        setFika(fika);
+        setMuseum(museum);
+        setCityWalks(cityWalks);
+        setBarChill(barChill);
+    }
+
+    public void setUserBio(String inputUserBio){
+        bio = inputUserBio;
+    }
     public void setFirstName(String inputFirstName) {
         firstName = inputFirstName;
     }
@@ -40,6 +58,10 @@ public class User {
     public void setBarChill(boolean inputBarChill) {
         barChill = inputBarChill;
     }
+
+
+
+
 }
 
 
