120af46edb4c5da5c3fb0cfdc7c82d5ae0aa7f1d
Goosedawg <120063500+Goosedawg@users.noreply.github.com>
2023-12-01 10:53:39 +0100

    här kommer user-klassen!

--- app/src/main/java/com/example/fluentfriend/User.java
+++ app/src/main/java/com/example/fluentfriend/User.java
@@ -22,51 +22,44 @@ public class User {
         System.out.println("Name: " + user1.firstName + user1.lastName);
     }
 
-    public void registerNewUser (String firstname, String lastName, String eMail, String password){
-        setFirstName(firstname);
-        setLastName(lastName);
-        setEmail(eMail);
-        setPassword(password);
+    protected void setUserInterests ( boolean fika, boolean museum, boolean cityWalks, boolean barChill){
+        setFika(fika);
+        setMuseum(museum);
+        setCityWalks(cityWalks);
+        setBarChill(barChill);
     }
 
-        public void setUserInterests ( boolean fika, boolean museum, boolean cityWalks, boolean barChill){
-            setFika(fika);
-            setMuseum(museum);
-            setCityWalks(cityWalks);
-            setBarChill(barChill);
-        }
-
-        public void setUserBio (String inputUserBio){
+    protected void setUserBio(String inputUserBio){
             bio = inputUserBio;
         }
-        public void setFirstName (String inputFirstName){
+    protected void setFirstName(String inputFirstName){
             firstName = inputFirstName;
         }
 
-        public void setLastName (String inputLastName){
+    protected void setLastName(String inputLastName){
             lastName = inputLastName;
         }
 
-        public void setEmail (String inputEmail){
+    protected void setEmail(String inputEmail){
             eMail = inputEmail;
         }
 
-        public void setPassword (String inputPassword){
+    protected void setPassword(String inputPassword){
             password = inputPassword;
         }
 
-        public void setFika ( boolean inputFika){
+    protected void setFika(boolean inputFika){
             fika = inputFika;
         }
-        public void setMuseum ( boolean inputMuseum){
+    protected void setMuseum(boolean inputMuseum){
             museum = inputMuseum;
         }
 
-        public void setCityWalks ( boolean inputCityWalks){
+    protected void setCityWalks(boolean inputCityWalks){
             cityWalks = inputCityWalks;
         }
 
-        public void setBarChill ( boolean inputBarChill){
+    protected void setBarChill(boolean inputBarChill){
             barChill = inputBarChill;
         }
 
