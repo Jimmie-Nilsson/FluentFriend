b5ccaa60430b02100519d8d966368acc11b7bdcb
move18 <snoozie@hotmail.se>
2023-12-04 21:22:31 +0100

    Backend code on signup WIP

--- app/src/main/java/com/example/fluentfriend/SignUp.java
+++ app/src/main/java/com/example/fluentfriend/SignUp.java
@@ -29,23 +29,28 @@ public class SignUp extends AppCompatActivity {
         repeatPassword = findViewById(R.id.signup_RepeatPassword);
         signUpBtn = findViewById(R.id.signup_btnSignUp);
         signUpBtn.setOnClickListener(view -> {
-            if (isInputCorrect()){
-               boolean exists = MainActivity.addNewUser(firstName.getText().toString(),lastName.getText().toString(),email.getText().toString(),password.getText().toString());
-               if (exists){
-                   Intent intent = new Intent(SignUp.this, HomePage.class);
-                   startActivity(intent);
-               }else {
-                   Toast.makeText(SignUp.this, "An account with this Email already exists", Toast.LENGTH_SHORT).show();
-               }
-            }else {
-                Toast.makeText(SignUp.this, "Wrong Input", Toast.LENGTH_LONG).show();
+            if (!isInputCorrect()) {
+                sendErrorMessage("All fields must be filled");
+                return;
             }
+            boolean exists = MainActivity.addNewUser(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
+            if (!exists) {
+                sendErrorMessage("An account with this Email already exists");
+                return;
+            }
+            Intent intent = new Intent(SignUp.this, HomePage.class);
+            startActivity(intent);
         });
     }
-    protected boolean isInputCorrect(){
-        if (firstName.getText().toString().trim().isEmpty() || lastName.getText().toString().trim().isEmpty() || email.getText().toString().trim().isEmpty() || password.getText().toString().trim().isEmpty() || repeatPassword.getText().toString().trim().isEmpty()){
+
+    protected boolean isInputCorrect() {
+        if (firstName.getText().toString().trim().isEmpty() || lastName.getText().toString().trim().isEmpty() || email.getText().toString().trim().isEmpty() || password.getText().toString().trim().isEmpty() || repeatPassword.getText().toString().trim().isEmpty()) {
             return false;
         }
         return password.getText().toString().equals(repeatPassword.getText().toString());
+    }
+
+    protected void sendErrorMessage(String message) {
+        Toast.makeText(SignUp.this, message, Toast.LENGTH_LONG).show();
     }
 }
