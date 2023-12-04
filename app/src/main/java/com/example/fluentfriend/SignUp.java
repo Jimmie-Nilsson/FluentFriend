0aed8e123d74e404a486c8a31aaff3fc620874a5
move18 <snoozie@hotmail.se>
2023-12-04 19:42:36 +0100

    Fixed changing button colors

--- app/src/main/java/com/example/fluentfriend/SignUp.java
+++ app/src/main/java/com/example/fluentfriend/SignUp.java
@@ -1,14 +1,13 @@
 package com.example.fluentfriend;
 
-
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_sign_up);
     }
 }
