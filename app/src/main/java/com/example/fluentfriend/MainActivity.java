5290d712375233636583d72f610af204daa4ffeb
kalleb17 <kalleberglund800@gmail.com>
2023-12-04 17:21:02 +0100

    Yes baby

--- app/src/main/java/com/example/fluentfriend/MainActivity.java
+++ app/src/main/java/com/example/fluentfriend/MainActivity.java
@@ -2,22 +2,43 @@ package com.example.fluentfriend;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button signInBtn;
+    private Button btnLogIn;
+    private Button btnCreateAccount;
+
+    private TextInputEditText text; // ???
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signInBtn = (Button) findViewById(R.id.button2);
-        signInBtn.setOnClickListener(view -> {
+        btnCreateAccount = (Button) findViewById(R.id.main_btnCreateAccount);
+        btnLogIn = (Button) findViewById(R.id.main_btnLogIn);
+        //text = (E) findViewById(R.id.main_emailField) ;
+
+
+
+        btnCreateAccount.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SignUp.class);
+            startActivity(intent);
+        });
+
+        btnLogIn.setOnClickListener(view -> {
+            // Skriv kod för att verifera inloggingen.
+
+
+
+
             Intent intent = new Intent(MainActivity.this, HomePage.class);
             startActivity(intent);
-
         });
+
     }
 }
