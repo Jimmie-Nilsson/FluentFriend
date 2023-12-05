dcec44f96e3a70775798415d5180dcba997a7c39
kalleb17 <kalleberglund800@gmail.com>
2023-12-05 12:55:40 +0100

    Uppdaterad

--- app/src/main/java/com/example/fluentfriend/HomePage.java
+++ app/src/main/java/com/example/fluentfriend/HomePage.java
@@ -2,6 +2,7 @@ package com.example.fluentfriend;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ public class HomePage extends AppCompatActivity {
     private User user;
 
     private Button btn;
+    private TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,17 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         btn = (Button) findViewById(R.id.btn_prfile);
+        welcomeText = (TextView) findViewById(R.id.homepage_welcomeText);
+        welcomeText.setText("Welcome " + user.getFirstName() + " " + user.getLastName());
 
         btn.setOnClickListener(view -> {
             Intent intent = new Intent(HomePage.this, UserProfile.class);
             startActivity(intent);
         });
 
+    }
+
+    public  void setUser(User user){
+        this.user = user;
     }
 }
--- app/src/main/java/com/example/fluentfriend/MainActivity.java
+++ app/src/main/java/com/example/fluentfriend/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView email;
     private  TextView password;
     private  User user;
+    private HomePage homePage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
             user = userList.get(inputEmail);
 
             if (inputPassword.equals(user.getPassword())) {
+                homePage.setUser(user);
                 Intent intent = new Intent(MainActivity.this, HomePage.class);
                 startActivity(intent);
             } else {
