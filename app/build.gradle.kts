a92588a40e5f6e82f63e97377569f70161a55b43
move18 <snoozie@hotmail.se>
2023-12-06 13:39:14 +0100

    FireBase DB and GPS Work in progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("com.android.application")
+
+
+    id("com.google.gms.google-services") version "4.4.0" apply false
+
 }
 
 android {
@@ -30,10 +34,16 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
