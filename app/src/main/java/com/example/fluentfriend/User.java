082f5b12f1f0ecee0c3226b2d62e53dc10497a83
kalleb17 <kalleberglund800@gmail.com>
2023-12-08 11:54:15 +0100

    Added Gender as varibel to User + get/set method. Addaed a second constuctor to User class for all the varibels that we will use when we get the data from the DB.
    // Kalle

--- app/src/main/java/com/example/fluentfriend/User.java
+++ app/src/main/java/com/example/fluentfriend/User.java
@@ -6,6 +6,7 @@ public class User implements java.io.Serializable {
     private String email;
     private String password;
     private String biography;
+    private String gender;
     private boolean fika;
     private boolean museum;
     private boolean cityWalks;
@@ -16,46 +17,40 @@ public class User implements java.io.Serializable {
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+
     }
-    protected void setUserBiography(String newBiography){
-            biography = newBiography;
-        }
-    protected void setFirstName(String inputFirstName){
-            firstName = inputFirstName;
-        }
-    protected void setLastName(String inputLastName){
-            lastName = inputLastName;
-        }
-    protected void setEmail(String inputEmail){
-            email = inputEmail;
-        }
-    protected void setPassword(String inputPassword){
-            password = inputPassword;
-        }
+    public User(String firstName, String lastName, String email, String password, String bio, boolean fika, boolean museum, boolean cityWalks, boolean bar, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.biography = bio;
+        this.fika = fika;
+        this.museum = museum;
+        this.cityWalks = cityWalks;
+        this.bar = bar;
+        this.gender = gender;
+    }
+    public void setUserBiography(String newBiography){biography = newBiography;}
+    public void setFirstName(String inputFirstName){firstName = inputFirstName;}
+    public void setLastName(String inputLastName){lastName = inputLastName;}
+    public void setEmail(String inputEmail){email = inputEmail;}
+    public void setPassword(String inputPassword){password = inputPassword;}
+    public void setGender(String gender){this.gender = gender;}
     public String getPassword() {return this.password;}
     public String getFirstName() {return this.firstName;}
     public String getLastName(){return this.lastName;}
     public String getEmail(){return this.email;}
-    public String getBiography() {
-        return this.biography;
-    }
-
-    protected void setFikaChecked(boolean checkBoxChecked){
-        fika = checkBoxChecked;
-    }
-    protected void setMuseumChecked(boolean checkBoxChecked){
-        museum = checkBoxChecked;
-    }
-    protected void setCityWalksChecked(boolean checkBoxChecked){
-        cityWalks = checkBoxChecked;
-    }
-    protected void setBarChecked(boolean checkBoxChecked){
+    public String getGender(){return this.gender;}
+    public String getBiography() {return this.biography;}
+    public void setFikaChecked(boolean checkBoxChecked){fika = checkBoxChecked;}
+    public void setMuseumChecked(boolean checkBoxChecked){museum = checkBoxChecked;}
+    public void setCityWalksChecked(boolean checkBoxChecked){cityWalks = checkBoxChecked;}
+    public void setBarChecked(boolean checkBoxChecked){
         bar = checkBoxChecked;
     }
-
     public boolean isFikaChecked() {return this.fika;}
     public boolean isMuseumChecked() {return this.museum;}
     public boolean isBarChecked() {return this.bar;}
     public boolean isCityWalksChecked() {return this.cityWalks;}
-
 }
