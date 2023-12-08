adfd9d10983c646fa364fcf8d48ec4a3031f96ad
kalleb17 <kalleberglund800@gmail.com>
2023-12-08 10:05:02 +0100

    Thinks the connections to Db works now!

--- app/src/main/java/com/example/fluentfriend/SignUp.java
+++ app/src/main/java/com/example/fluentfriend/SignUp.java
@@ -9,9 +9,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.*;
-
-import java.util.HashMap;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
@@ -21,6 +20,8 @@ public class SignUp extends AppCompatActivity {
     EditText password;
     EditText repeatPassword;
     Button signUpBtn;
+
+
 
 
     @Override
@@ -33,50 +34,29 @@ public class SignUp extends AppCompatActivity {
         password = findViewById(R.id.signup_Password);
         repeatPassword = findViewById(R.id.signup_RepeatPassword);
         signUpBtn = findViewById(R.id.signup_btnSignUp);
-        FirebaseApp.initializeApp(this);
-        DatabaseReference myref = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance("https://fluent-friend-dad39-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef =db.getReference("test");
+        myRef.setValue("Hello Jimmie");
         signUpBtn.setOnClickListener(view -> {
             if (!isInputCorrect()) {
                 sendErrorMessage("All fields must be filled");
                 return;
             }
-
             User user = new User(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
+            boolean exists = MainActivity.addNewUser(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
+            if (!exists) {
+                sendErrorMessage("An account with this Email already exists");
+                return;
+            }
+            //myref.child("users").child(user.getEmail()).setValue(user);
 
-            DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
-
-            ValueEventListener postListener = new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-
-                   if (!dataSnapshot.child("users").child(user.getEmail()).exists()){
-                       HashMap<String,User> list = new HashMap<>();
-                       list.put(user.getEmail(),user);
-                       usersRef.child("users").setValue(list);
-                       Intent intent = new Intent(SignUp.this, HomePage.class);
-                       startActivity(intent);
-                   }else {
-                       sendErrorMessage("User exists!");
-                   }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    System.out.println(databaseError.toString());
-                }
-            };
-//
-//            if (rootRef.child("users").child(user.getEmail()).getKey() == null){
-//                myref.child("users").child(user.getEmail()).setValue(user);
-//                Intent intent = new Intent(SignUp.this, HomePage.class);
-//                startActivity(intent);
-//            }else {
-//                sendErrorMessage("A user with this email already exists!");
-//            }
+             Intent intent = new Intent(SignUp.this, HomePage.class);
+            startActivity(intent);
         });
         }
 
-    private boolean isInputCorrect() { // make a method for the toString().trim().isEmpty() ch
+    private boolean isInputCorrect() { // make a method for the toString().trim().isEmpty() checks
         if (firstName.getText().toString().trim().isEmpty() || lastName.getText().toString().trim().isEmpty() || email.getText().toString().trim().isEmpty() || password.getText().toString().trim().isEmpty() || repeatPassword.getText().toString().trim().isEmpty()) {
             return false;
         }
