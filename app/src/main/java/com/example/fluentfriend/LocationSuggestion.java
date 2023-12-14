44ea78d71067add3e8464ed17f0f16db9d4481e8
Goosedawg <120063500+Goosedawg@users.noreply.github.com>
2023-12-14 16:57:33 +0100

    Can now open google maps based on location with searches

--- app/src/main/java/com/example/fluentfriend/LocationSuggestion.java
+++ app/src/main/java/com/example/fluentfriend/LocationSuggestion.java
@@ -2,7 +2,11 @@ package com.example.fluentfriend;
 
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.maps.GeoApiContext;
 import com.google.maps.PlacesApi;
@@ -51,7 +55,15 @@ public class LocationSuggestion extends AppCompatActivity {
         context = new GeoApiContext.Builder()
                 .apiKey(apiKey)
                 .build();
+
         fetchNearbyPlaces();
+        Button findNearbyPlacesButton = findViewById(R.id.find_nearby_places);
+        findNearbyPlacesButton.setOnClickListener(view ->
+        {fetchNearbyPlaces();
+        });
+
+        Button openGoogleMapsButton = findViewById(R.id.open_google_maps); // replace with your actual button ID
+        openGoogleMapsButton.setOnClickListener(view -> openGoogleMaps(midpoint));
     }
     private Location getMiddleDistanceBetweenUsers(Location loc1, Location loc2) {
         double lat = (loc1.getLatitude() + loc2.getLatitude()) / 2;
@@ -61,7 +73,22 @@ public class LocationSuggestion extends AppCompatActivity {
         midpoint.setLongitude(longitude);
         return midpoint;
     }
+    private void openGoogleMaps(Location location) {
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("geo:" + location.getLatitude() + "," + location.getLongitude() + "?q=cafes");
 
+        // Create an Intent to open Google Maps at the specified location
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Attempt to start an activity that can handle the Intent
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        } else {
+            // Show an error if Google Maps is not installed
+            Toast.makeText(this, "Google Maps is not installed.", Toast.LENGTH_LONG).show();
+        }
+    }
     private void fetchNearbyPlaces() {
         new Thread(() -> {
             try {
@@ -71,10 +98,17 @@ public class LocationSuggestion extends AppCompatActivity {
                         .type(PlaceType.CAFE)
                         .await()
                         .results;
-
                 // Now you need to handle the results on the UI thread
                 runOnUiThread(() -> {
-                    // Update your UI with the results here
+                    if (results.length > 0) {
+                        // Update your UI with the results here
+                        // e.g., display the name of the first result in a TextView
+                        TextView resultView = findViewById(R.id.resultTextView);
+                        resultView.setText(results[0].name);
+                    } else {
+                        // Show a message if no results were found
+                        Toast.makeText(this, "No nearby cafes found.", Toast.LENGTH_LONG).show();
+                    }
                 });
             } catch (Exception e) {
                 e.printStackTrace();
@@ -82,4 +116,5 @@ public class LocationSuggestion extends AppCompatActivity {
             }
         }).start();
     }
+
 }
