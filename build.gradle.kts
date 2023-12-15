e571223bccae0985761a560d95d4d510ae8f27d5
Goosedawg <120063500+Goosedawg@users.noreply.github.com>
2023-12-15 14:55:02 +0100

    Added functionality for push notices and instant messaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,30 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0") // specify the correct version for your project
+        classpath("com.google.gms:google-services:4.4.0")
+
+        // Add other classpaths if necessary
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+task("clean", type = Delete::class) {
+    delete(rootProject.buildDir)
 }
